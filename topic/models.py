c193dccc36243eae4c5fe618b9cf6a7b732da022
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-02 21:51:09 +0200

    Added topic model to models.py

--- topic/models.py
+++ topic/models.py
@@ -1,3 +1,12 @@
+# -*- encoding:utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """ A class for storing instance topic """
+    title = models.CharField(max_length=100)
+    question = models.TextField()
+    article_date = models.DateTimeField()
+
+    def __unicode__(self):
+        return ''.join(self.title)
