e04f867dd20f2a0474cbe23395b57a886029f716
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-02 21:53:50 +0200

    Changed DATABASES variable. Use postgresql database.

--- simpleSO/settings.py
+++ simpleSO/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'simpleSO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stackoverflow',
+        'USER': 'denis',
+        'PASSWORD': 'xjbc1u64',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
