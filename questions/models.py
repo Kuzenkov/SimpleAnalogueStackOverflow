d23a27b0352e72452945107efaa64791f7bc1dde
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 13:45:06 +0200

    Added function get_absolute_url()

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,29 @@
+# -*- encoding:utf-8 -*-
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Question(models.Model):
+
+    class Meta:
+        db_table = "questions"
+
+    title = models.CharField(max_length=150)
+    question = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return ' '.join(self.title)
+
+    def get_absolute_url(self):
+        return reverse('contacts-view', kwargs={'pk': self.id})
+
+
+class Comments(models.Model):
+    """ Contain user comments for question """
+
+    class Meta:
+        db_table = "comments"
+
+    comment_text = models.TextField(verbose_name="comment")
+    comments_question = models.ForeignKey(Question)
