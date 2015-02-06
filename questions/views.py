3084758934e8ff1b76c3527cd78402291944dc83
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 13:46:04 +0200

    Added class QuestionView for detail view

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import ListView, DetailView
+from questions.models import Question
 
-# Create your views here.
+
+class QuestionsList(ListView):
+    """ Class for displaying list of questions """
+    model = Question
+    template_name = 'question_list.html'
+    context['title'] = 'title'
+
+
+class QuestionView(DetailView):
+    model = Question
+    template_name = 'question.html'
