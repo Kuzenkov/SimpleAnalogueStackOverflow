a33dc09fd6b2ee93ecadcf61f350b4980da1ccd5
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 13:49:05 +0200

    Added url for display question

---
+++ questions/urls.py
@@ -0,0 +1,10 @@
+# -*- encoding:utf-8 -*-
+from django.conf.urls import patterns, include, url
+import questions.views
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', questions.views.QuestionsList.as_view(), name='questions-list'),
+    url(r'^(?P<pk>\d+)/$', questions.views.QuestionView.as_view(), name='contacts-view',),
+)
