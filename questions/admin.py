e4aa820df639789b5a6da4e4081628f88100b49b
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 17:38:55 +0200

    Added question and comment model for administration

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from questions.models import Question, Comments
 
-# Register your models here.
+
+class QuestionInline(admin.StackedInline):
+    """Class for adding comments to question."""
+    model = Comments
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """Class for administration Question objects."""
+    inlines = [QuestionInline]
+    list_filter = ['created_on']
+
+admin.site.register(Question, QuestionAdmin)
