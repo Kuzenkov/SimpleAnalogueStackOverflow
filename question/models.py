b74aee9ed6aa465aa59fff55e01dd7984fd92bda
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 12:03:23 +0200

    Added model question, Added model comments which related with question

--- question/models.py
+++ question/models.py
@@ -1,3 +1,25 @@
+# -*- encoding:utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+
+    class Meta():
+        db_table = "question"
+
+    title = models.CharField(max_length=150)
+    question = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return ' '.join(self.title)
+
+
+class Comments(models.Model):
+    """ Contain user comments for question """
+
+    class Meta():
+        db_table = "comments"
+
+    comment_text = models.TextField(verbose_name="comment")
+    comments_question = models.ForeignKey(Question)
