9fe02f9113dcf859b0a552eac5a4197c5b219a7f
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 13:23:10 +0200

    Changedfolder name from question to qeuestions

--- questions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-import question.views
-
-
-urlpatterns = patterns('',
-    url(r'^', 'question.views.QuestionsList.as_view()', name='questions-list'),
-)
--- questions/views.py
+++ questions/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render, render_to_response
-from django.views.generic import ListView
-from question.models import Question
+from django.shortcuts import render
 
-
-class QuestionsList(ListView):
-    """ Class for displaying list of questions """
-    model = Question
-    template_name = 'question_list.html'
+# Create your views here.
