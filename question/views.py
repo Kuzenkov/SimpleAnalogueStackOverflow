bca02999df3ca16c085ab52ce3a4b82ea5c6f8d9
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 12:22:37 +0200

    Created class QuestionsView for displaying list of questions

--- question/views.py
+++ question/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import ListView
+from question.model import Question
 
-# Create your views here.
+
+class QuestionsView(ListView):
+    """ Class for displaying list of questions """
+    model = Question
+    template_name = 'question_list.html'
