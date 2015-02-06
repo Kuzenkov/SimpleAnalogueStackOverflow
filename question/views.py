25aadf11b6d8790aa1fdd973698cba0ffe7d1d72
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 12:26:43 +0200

    Changed class name from QuestionsViews to QuestionsList

--- question/views.py
+++ question/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from question.model import Question
 
 
-class QuestionsView(ListView):
+class QuestionsList(ListView):
     """ Class for displaying list of questions """
     model = Question
     template_name = 'question_list.html'
