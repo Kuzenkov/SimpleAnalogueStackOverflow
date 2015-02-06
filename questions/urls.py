61d3b6beb73dd4ee8a756eb7e06c67df01034ab7
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-06 17:42:42 +0200

    Changed template name from contacts-view to question-view

--- questions/urls.py
+++ questions/urls.py
@@ -6,5 +6,5 @@ import questions.views
 urlpatterns = patterns(
     '',
     url(r'^$', questions.views.QuestionsList.as_view(), name='questions-list'),
-    url(r'^(?P<pk>\d+)/$', questions.views.QuestionView.as_view(), name='contacts-view',),
+    url(r'^(?P<pk>\d+)/$', questions.views.QuestionView.as_view(), name='question-view',),
 )
