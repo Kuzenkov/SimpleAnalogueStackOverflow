02b97775e4f099a61ebea780f320f86b339818c9
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-10 13:14:37 +0200

    Added mathod get_context_data() for dislpaying question answers

--- questions/views.py
+++ questions/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, render_to_response
 from django.views.generic import ListView, DetailView
-from questions.models import Question
+from questions.models import Question, Comments
 
 
 class QuestionsList(ListView):
     """ Class for displaying list of questions """
     model = Question
+    paginate_by = 10
     template_name = 'question_list.html'
-
 
 
 class QuestionView(DetailView):
     model = Question
     template_name = 'question.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionView, self).get_context_data(**kwargs)
+        context['comments'] = Comments.objects.filter(comments_question_id=self.get_object().pk)
+        return context
