a1f4581257457d07fe7a6025662f01780bf48597
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-11 20:26:46 +0200

    Added variable form_class, override mathods post, get_success_url, added variable content['form'] to method get_context_data

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.views.generic import ListView, DetailView
-from questions.models import Question, Comments
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import FormView, CreateView, ModelFormMixin, ProcessFormView
+from questions.models import Question, Answer
+from forms import AnswerForm
 
 
 class QuestionsList(ListView):
@@ -10,11 +13,20 @@ class QuestionsList(ListView):
     template_name = 'question_list.html'
 
 
-class QuestionView(DetailView):
+class QuestionView(DetailView, ModelFormMixin, ProcessFormView):
     model = Question
+    form_class = AnswerForm
     template_name = 'question.html'
+
+    def get_success_url(self):
+        return reverse('question-view', kwargs={'pk': self.get_object().pk})
 
     def get_context_data(self, **kwargs):
         context = super(QuestionView, self).get_context_data(**kwargs)
-        context['comments'] = Comments.objects.filter(comments_question_id=self.get_object().pk)
+        context['answers'] = Answer.objects.filter(answers_question_id=self.get_object())
+        context['form'] = self.form_class
         return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = Answer(answers_question_id=self.get_object().pk)
+        return super(QuestionView, self).post(request, *args, **kwargs)
