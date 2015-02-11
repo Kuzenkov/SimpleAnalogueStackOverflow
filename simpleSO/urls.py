23cf259f54e4a9611ccd34d274649599155a673a
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-11 21:44:33 +0200

    Changed indent in 'name='password_reset_confirm'),' line

--- simpleSO/urls.py
+++ simpleSO/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns(
     url(r'^accounts/password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm,
-         name='password_reset_confirm'),
+        name='password_reset_confirm'),
 
     url(r'', include('social_auth.urls')),
 
