c76c7df1e55aee4b6176715f312539b3d99019f7
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-11 21:35:12 +0200

    Added widgets to AnswerForm, created class QuestionForm for add new question

--- questions/forms.py
+++ questions/forms.py
@@ -1,8 +1,28 @@
-from django.forms import ModelForm
-from questions.models import Answer
+from django.forms import ModelForm, Textarea
+from questions.models import Answer, Question
 
 
 class AnswerForm(ModelForm):
     class Meta:
         model = Answer
         fields = ['answer_text']
+        widgets = {
+            'answer_text': Textarea(attrs={'cols': 70, 'rows': 10}),
+        }
+
+
+class QuestionForm(ModelForm):
+
+    class Meta:
+        model = Question
+        fields = ['title', 'question']
+
+    def __init__(self, *args, **kwargs):
+        self.owner = kwargs['initial']['owner']
+        super(QuestionForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        obj = super(QuestionForm, self).save(False)
+        obj.owner = self.owner
+        commit and obj.save()
+        return obj
