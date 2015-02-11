c3afc49cb723a5691cecb3fac7a0b55bce69be5c
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-11 21:38:54 +0200

    Added uorl for adding new question

--- questions/urls.py
+++ questions/urls.py
@@ -6,5 +6,6 @@ import questions.views
 urlpatterns = patterns(
     '',
     url(r'^$', questions.views.QuestionsList.as_view(), name='questions-list'),
-    url(r'^(?P<pk>\d+)/$', questions.views.QuestionView.as_view(), name='question-view',),
+    url(r'^(?P<pk>\d+)/$', questions.views.QuestionView.as_view(), name='question-view'),
+    url(r'^new-question/$', questions.views.QuestionCreate.as_view(), name='new-question'),
 )
