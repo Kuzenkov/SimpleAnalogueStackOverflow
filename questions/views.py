edf51a33fe506df7a4d5cde027b80d0cfe0e110c
Denis Kuzenkov <denkuzenkov@gmail.com>
2015-02-16 20:15:25 +0200

    Added method get_query_set in QuestionList for pagination, pagination page = 10; Changed handle form method to valid_form; Added valid_form, get_succsse_url mathods, deleted get_init method;

--- questions/views.py
+++ questions/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.generic.edit import CreateView, ModelFormMixin, ProcessFormView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from questions.models import Question, Answer
 from forms import AnswerForm, QuestionForm
+from questions import signals
 
 
 class LoggedInMixin(object):
@@ -17,8 +20,23 @@ class LoggedInMixin(object):
 class QuestionsList(ListView):
     """ Class for displaying list of questions """
     model = Question
-    paginate_by = 10
     template_name = 'question_list.html'
+
+    def get_queryset(self):
+        question_list = Question.objects.filter()
+
+        paginator = Paginator(question_list, 10)
+        page = self.request.GET.get('page')
+
+        try:
+            questions = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            questions = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            questions = paginator.page(paginator.num_pages)
+        return questions
 
 
 class QuestionView(DetailView, ModelFormMixin, ProcessFormView):
@@ -38,9 +56,12 @@ class QuestionView(DetailView, ModelFormMixin, ProcessFormView):
         context['form'] = self.form_class
         return context
 
-    def post(self, request, *args, **kwargs):
-        self.object = Answer(answers_question_id=self.get_object().pk)
-        return super(QuestionView, self).post(request, *args, **kwargs)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.answers_question_id = self.get_object().pk
+        self.object.save()
+        signals.post_save.send(sender=Answer, instance=self.get_object(), answer=self.object.answer_text)
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class QuestionCreate(LoggedInMixin, CreateView):
@@ -48,6 +69,17 @@ class QuestionCreate(LoggedInMixin, CreateView):
     template_name = 'new_question.html'
     form_class = QuestionForm
 
-    def get_initial(self):
+    def get_success_url(self):
+        return reverse('question-view', kwargs={'pk': self.object.id})
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    # another method create object with owner parameter
+    """def get_initial(self):
         self.initial.update({'owner': self.request.user})
         return self.initial
+    """
